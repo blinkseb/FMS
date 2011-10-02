b10ef8e6b49fa4e1e4fe91566246d75e970ca83f
Sébastien Brochet <blinkseb@xbmc.org>
2011-10-02 19:42:39 +0200

    added: LICENSE informations

--- Program.cs
+++ Program.cs
@@ -1,4 +1,13 @@
-﻿using System;
+﻿/*
+ * This file is part of the FMS project
+ *
+ * (c) 2011 Sébastien Brochet <blinkseb-nospam-@madalynn.eu>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+using System;
 using System.Windows.Forms;
 
 namespace FMS
